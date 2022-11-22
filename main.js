c9016d22b964872f04ee67fa07b62678fe5eb815
ezerdigital <abjanan@gmail.com>
2022-11-22 13:05:52 -0500

    first_commit_for_Classroom_Github

---
+++ main.js
@@ -0,0 +1,31 @@
+const movies = [ 
+	{
+		id: 1, 
+		title: "The Notebook",
+		year: 2004,
+		genres: ["romance"],
+		country: "USA"
+	},
+	{
+		id: 2, 
+		title: "Parasite",
+		year: 2019,
+		genres: ["thriler","drama", "comedy"],
+		country: "Korea"
+	}, 
+	{
+		id: 3, 
+		title: "Pulp Fiction",
+		year: 2019,
+		genres: ["crime","drama"],
+		country: "USA"
+	},
+	{
+		id: 4, 
+		title: "Knife in the Water",
+		year: 1962,
+		genres: ["thriler","drama"],
+		country: "Poland"
+	} ];
+
+console.log(movies)
---
+++ testing.js
@@ -0,0 +1,47 @@
+const movies = [ 
+	{
+		id: 1, 
+		title: "The Notebook",
+		year: 2004,
+		genres: ["romance"],
+		country: "USA"
+	},
+	{
+		id: 2, 
+		title: "Parasite",
+		year: 2019,
+		genres: ["thriler","drama", "comedy"],
+		country: "Korea"
+	}, 
+	{
+		id: 3, 
+		title: "Pulp Fiction",
+		year: 2019,
+		genres: ["crime","drama"],
+		country: "USA"
+	},
+	{
+		id: 4, 
+		title: "Knife in the Water",
+		year: 1962,
+		genres: ["thriler","drama"],
+		country: "Poland"
+	} ];
+
+console.log(movies[3].title);
+
+const x = 1;
+const y = 2;
+
+const add = (n1,n2) => {
+	return n1+n2;
+};
+
+add(x,y);
+console.log(x+y);
+
+
+
+
+
+
